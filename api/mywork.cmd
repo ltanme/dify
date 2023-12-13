645479515dfb6a30ff8e36f995bd23dd764f1524
ltanme <382261604@qq.com>
2023-12-13 08:51:48 +0000

    增加脚本

---
+++ api/mywork.cmd
@@ -0,0 +1,9 @@
+@echo off
+REM 切换到虚拟环境目录
+cd /d "E:\workProject\dify-0.3.33\env\dify-python310\Scripts"
+
+REM 激活虚拟环境
+call activate.bat
+
+REM 回到工作目录
+cd /d "E:\workProject\dify-0.3.33\dify\api"
---
+++ api/mywork.ps1
@@ -0,0 +1,8 @@
+# 切换到虚拟环境目录
+Set-Location "E:\SDAI\python310\venv\Scripts"
+
+# 激活虚拟环境
+& ".\activate.ps1"
+
+# 回到工作目录
+Set-Location "E:\workProject\dify-0.3.33\dify\api"
